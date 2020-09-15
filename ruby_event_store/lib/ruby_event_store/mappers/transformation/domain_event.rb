mpokrywka/rails_event_store

Adapt mappers to timestamp passed in SerializedRecord.

ruby_event_store/lib/ruby_event_store/mappers/transformation/domain_event.rb
@@ -7,16 +7,17 @@ module RubyEventStore
         def dump(domain_event)
           Item.new(
             event_id:   domain_event.event_id,
-            metadata:   domain_event.metadata.to_h,
+            metadata:   domain_event.metadata.to_h.reject { |k, _| k.eql?(:timestamp) },
             data:       domain_event.data,
-            event_type: domain_event.type
+            event_type: domain_event.type,
+            timestamp:  domain_event.timestamp,
           )
         end
 
         def load(item)
           Object.const_get(item.event_type).new(
             event_id: item.event_id,
-            metadata: item.metadata,
+            metadata: item.metadata.merge(timestamp: item.timestamp),
             data:     item.data
           )
         end

sfo-den/railseventstore

ruby_event_store/lib/ruby_event_store/mappers/transformation/domain_event.rb
@@ -7,17 +7,18 @@ module RubyEventStore
         def dump(domain_event)
           Record.new(
             event_id:   domain_event.event_id,
-            metadata:   domain_event.metadata.to_h,
+            metadata:   domain_event.metadata.to_h.reject { |k, _| k.eql?(:timestamp) },
             data:       domain_event.data,
-            event_type: domain_event.event_type
+            event_type: domain_event.event_type,
+            timestamp:  domain_event.timestamp,
           )
         end
 
         def load(record)
           Object.const_get(record.event_type).new(
             event_id: record.event_id,
-            metadata: record.metadata,
-            data:     record.data
+            metadata: record.metadata.merge(timestamp: record.timestamp),
+            data:     record.data,
           )
         end
       end